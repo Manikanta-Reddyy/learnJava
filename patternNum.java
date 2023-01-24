effd898aa66408ffcd25372821610d4d226448f7
Manikanta Reddy <manikantareddykomatla291@gmail.com>
2023-01-24 15:44:41 +0530

    v2:added signup feature

---
+++ patternNum.java
@@ -0,0 +1,12 @@
+import javax.sql.rowset.spi.SyncResolver;
+
+import java.util.*;
+public class patternNum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String  i = sc.nextLine();
+        System.out.println(i+" is a Good boy");
+
+    }
+}
