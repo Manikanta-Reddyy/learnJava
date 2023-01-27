7d271a5130cb57e0e07f442fcb889712d2e36882
Manikanta-Reddyy <manikantareddykomatla291@gmail.com>
2023-01-28 00:24:45 +0530

    Initial commit

--- Loops.java
+++ Loops.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class Loops {
     public static void main(String[] args) {
-        // for(int i=0; i < 10; i++) {
-        //     System.out.println(i);
-        // }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i <= 10; i++) {
+            System.out.println(i*n);
+        }
 
         // int i =0;
         // while(i<10) {
@@ -12,11 +14,11 @@ public class Loops {
         //     i=i++;
         // }
 
-        int i = 0;
-        do {
-            System.out.println(i);
-            i++;
-        } while (i < 10);
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 10);
 
 
 
